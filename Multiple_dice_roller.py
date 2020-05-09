88fad5e563a753d7349908437f2214bfd8257256
akenz1901 <62916294+akenz1901@users.noreply.github.com>
2020-05-09 11:43:00 +0100

    Multiple dice roller

---
+++ Multiple_dice_roller.py
@@ -0,0 +1,9 @@
+import random
+
+dice_rolls = 2
+dice_sum = 0
+for i in range(0,dice_rolls):
+    roll = random.randint(1,6)
+    dice_sum += roll
+    print(f'You rolled a {roll}')
+print(f'You have rolled a total of {dice_sum}')
