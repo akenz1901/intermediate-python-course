14bb1d436b29a853e7fc64d1f4f0321811dd8771
akenz1901 <62916294+akenz1901@users.noreply.github.com>
2020-05-10 19:34:28 +0100

    Adding_Condition

---
+++ Adding_condition.py
@@ -0,0 +1,15 @@
+import random
+
+dice_rolls = 2
+dice_sum = 0
+for i in range(0,dice_rolls):
+    roll = random.randint(1,6)
+    dice_sum += roll
+    
+if roll == 1:
+    print(f'You rolled {roll}! Critical fail')
+elif roll == 6:
+    print(f'You rolled {roll}! Critical Succes')
+else:
+    print(f'You rolled {roll}')
+print(f'You have rolled a total {dice_sum}')
