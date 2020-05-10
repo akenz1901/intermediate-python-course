270f2a47d8fc551556366d2c65f19b340e06191d
akenz1901 <62916294+akenz1901@users.noreply.github.com>
2020-05-10 20:11:58 +0100

    Tutorial complete

--- Adding_condition.py
+++ Adding_condition.py
@@ -5,7 +5,6 @@ dice_sum = 0
 for i in range(0,dice_rolls):
     roll = random.randint(1,6)
     dice_sum += roll
-    
 if roll == 1:
     print(f'You rolled {roll}! Critical fail')
 elif roll == 6:
